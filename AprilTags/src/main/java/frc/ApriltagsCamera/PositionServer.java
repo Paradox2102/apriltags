eb7198009849cf48d1ea42b590d72ccaa369f1d1
campad <84256232+campad@users.noreply.github.com>
2023-02-04 19:24:34 -0800

    Added path creation

--- AprilTags/src/main/java/frc/ApriltagsCamera/PositionServer.java
+++ AprilTags/src/main/java/frc/ApriltagsCamera/PositionServer.java
@@ -14,7 +14,12 @@ public class PositionServer implements NetworkReceiver {
     private double m_yPos = 0;
     private boolean m_newPos = true;
     private Object m_lock = new Object();
+    private Object m_recvLock = new Object();
     private Gyro m_gyro;
+
+    double m_testX = 10*12;
+    double m_testY = 0;
+    double m_angle = 90;
 
     public void start(Gyro gyro) {
         m_gyro = gyro;
@@ -27,6 +32,15 @@ public class PositionServer implements NetworkReceiver {
                 // Logger.log("PositionServer", 1, "connected=" + m_connected);
 				if (m_connected) {
 					Logger.log("PositionServer",-1, "Send position");
+
+                    {
+
+                        m_network.sendMessage(String.format("+%.2f %.2f %.2f\n", m_angle, m_testX, m_testY));
+
+                        m_testX += 1;
+                        m_testY += 0.5;
+                        m_angle += 1.0/5;
+                    }
 
                     double xPos;
                     double yPos;
@@ -60,17 +74,117 @@ public class PositionServer implements NetworkReceiver {
     }
 
     public void setPosition(double x, double y) {
-       synchronized (m_lock)
-       {
-           m_xPos = x;
-           m_yPos = y;
-           m_newPos = true;
-       }
+        synchronized (m_lock) {
+            m_xPos = x;
+            m_yPos = y;
+            m_newPos = true;
+        }
+    }
+
+    public class BezierData {
+        public double m_x1; // In feet
+        public double m_y1; // In feet
+        public double m_angle1; // In radians
+        public double m_l1; // In feet
+        public double m_x2; // In feet
+        public double m_y2; // In feet
+        public double m_angle2; // In radians
+        public double m_l2; // In Feet
+
+        public BezierData(double x1, double y1, double angle1, double l1, double x2, double y2, double angle2,
+                double l2) {
+            Logger.log("BezierData", 1, String.format("%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f,%0.2f", x1, y1, angle1, l1, x2, y2, angle2, l2));
+
+            m_x1 = x1;
+            m_y1 = y1;
+            m_angle1 = angle1;
+            m_l1 = l1;
+            m_x2 = x2;
+            m_y2 = y2;
+            m_angle2 = angle2;
+            m_l2 = l2;
+        }
+    }
+
+    private BezierData[] m_data;
+    private BezierData[] m_newData;
+    private int m_nBezier;
+
+    private void processPath(String path) {
+        int[] args = ApriltagsCamera.parseIntegers(path, 1);
+
+        if (args != null) {
+            m_nBezier = 0;
+            m_newData = new BezierData[args[0]];
+        }
+
+    }
+
+    private void processBezier(String bezier) {
+        if ((m_newData != null) && (m_nBezier < m_newData.length)) {
+            double[] arg = ApriltagsCamera.parseDouble(bezier, 8);
+            m_newData[m_nBezier] = new BezierData(arg[0], arg[1], arg[2], arg[3], arg[4], arg[5], arg[6], arg[8]);
+        }
+        else
+        {
+            m_newData = null;
+            Logger.log("PositionServer", 3, String.format("Invalid Bezier Command: %s", bezier));
+        }
+
+        m_nBezier++;
+    }
+
+    private void processEnd(String end) {
+        if (m_newData != null) {
+            if (m_nBezier == m_newData.length) {
+                synchronized (m_recvLock) {
+                    m_data = m_newData;
+                    m_newData = null;
+                }
+            }
+            else {
+                Logger.log("PositionServer", 3, String.format("Invalid Bezier Length: %d/%d", m_nBezier, m_newData.length));
+            }
+        }
     }
 
     @Override
-    public void processData(String command) {
+    public void processData(String data) {
+        Logger.log("PositionServer", -1, String.format("Data: %s", data));
 
+        switch (data.charAt(0)) {
+            case 'P': // path
+                processPath(data.substring(1).trim());
+                break;
+
+            case 'B': // Bezier curve
+                processBezier(data.substring(1).trim());
+                break;
+
+            case 'E': // End
+                processEnd(data.substring(1).trim());
+                break;
+
+            case 'k':   // keep alive
+                break;
+
+            default:
+                Logger.log("PositionServer", 3, String.format("Invalid command: %s", data));
+                break;
+        }
+    }
+
+    public BezierData[] getData()
+    {
+        BezierData[] data;
+
+        synchronized(m_recvLock)
+        {
+            data = m_data;
+            m_data = null;
+        }
+
+        return(data);
     }
 
     @Override
