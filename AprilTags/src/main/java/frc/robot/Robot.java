ea791ac4a7c0a18b2273b20ce3bc5b01ee31240a
campad <84256232+campad@users.noreply.github.com>
2023-02-15 13:08:56 -0800

    Apriltags Calibration

--- AprilTags/src/main/java/frc/robot/Robot.java
+++ AprilTags/src/main/java/frc/robot/Robot.java
@@ -18,9 +18,12 @@ import frc.ApriltagsCamera.ApriltagsCamera.ApriltagsCameraRegions;
 import frc.ApriltagsCamera.ApriltagsCamera.ApriltagsCameraRegions.RobotPos;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -32,12 +35,14 @@ public class Robot extends TimedRobot {
   private PositionServer m_posServer = new PositionServer();
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
     m_gyro.reset(0);
@@ -47,10 +52,13 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   int m_frameCount = 0;
@@ -59,20 +67,34 @@ public class Robot extends TimedRobot {
 
   ApriltagLocation tags[] = { new ApriltagLocation(12, 0, 324), new ApriltagLocation(0, 12, 324) };
 
+  private double normalizeAngle (double angle) {
+    angle = angle%360;
+    if (angle <= -180) {
+      angle += 360;
+    }
+    else if (angle >= 180) {
+      angle -=360;
+    }
+    return angle;
+  }
+
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
 
-    SmartDashboard.putNumber("yaw", m_gyro.getAngle());
-  
+    SmartDashboard.putNumber("yaw", normalizeAngle(m_gyro.getAngle()));
+
     ApriltagsCameraRegions regions = m_camera.getRegions();
 
     m_posServer.setAllianceColor(DriverStation.getAlliance() == DriverStation.Alliance.Red);
-    
+
     if (regions != null) {
       Logger.log("Robot", -1, String.format("nRegions = %d", regions.m_regions.size()));
       SmartDashboard.putNumber("nRegions", regions.m_regions.size());
@@ -84,27 +106,31 @@ public class Robot extends TimedRobot {
         SmartDashboard.putNumber("missing", ++m_missingCount);
       }
 
-      // RobotPos pos = regions.ComputeRobotPosition(tags, m_gyro.getAngle() * Math.PI / 180);
+      // RobotPos pos = regions.ComputeRobotPosition(tags, m_gyro.getAngle() * Math.PI
+      // / 180);
       // if (pos != null)
       // {
-      //   SmartDashboard.putNumber("xPos", pos.m_x);
-      //   SmartDashboard.putNumber("yPos", pos.m_y);
-      //   m_posServer.setPosition(pos.m_x, pos.m_y);
+      // SmartDashboard.putNumber("xPos", pos.m_x);
+      // SmartDashboard.putNumber("yPos", pos.m_y);
+      // m_posServer.setPosition(pos.m_x, pos.m_y);
       // }
 
       int i = 1;
       for (ApriltagsCameraRegion region : regions.m_regions) {
-        SmartDashboard.putNumber(String.format("Tag%d", i), region.m_tag);
-        SmartDashboard.putNumber(String.format("Dist%d", i), Math.sqrt(region.m_tvec[0] * region.m_tvec[0] +
-                                                                       region.m_tvec[1] * region.m_tvec[1] +
-                                                                       region.m_tvec[2] * region.m_tvec[2]));
-        SmartDashboard.putNumber(String.format("rx%d", i), region.m_rvec[0]);                                                                    
-        SmartDashboard.putNumber(String.format("ry%d", i), region.m_rvec[1]);                                                                    
-        SmartDashboard.putNumber(String.format("rz%d", i), region.m_rvec[2]);                                                                    
-        if (region.m_tag < 0) {
-          SmartDashboard.putNumber("invalid", ++m_invalidCount);
+        if (region.m_tag == 1) {
+
+          SmartDashboard.putNumber(String.format("Tag%d", i), region.m_tag);
+          SmartDashboard.putNumber(String.format("Dist%d", i), Math.sqrt(region.m_tvec[0] * region.m_tvec[0] +
+              region.m_tvec[1] * region.m_tvec[1] +
+              region.m_tvec[2] * region.m_tvec[2]));
+          SmartDashboard.putNumber(String.format("rx%d", i), region.m_rvec[0]);
+          SmartDashboard.putNumber(String.format("ry%d", i), region.m_rvec[1]);
+          SmartDashboard.putNumber(String.format("rz%d", i), region.m_rvec[2]);
+          if (region.m_tag < 0) {
+            SmartDashboard.putNumber("invalid", ++m_invalidCount);
+          }
+          i++;
         }
-        i++;
       }
     }
   }
@@ -118,7 +144,10 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {
   }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
